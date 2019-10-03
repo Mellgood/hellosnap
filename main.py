d14bf554217a04fe649093329fb79df44cf5797c
Carlo Centofanti <carlocentos@gmail.com>
2019-10-03 18:04:03 +0200

    asd

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
+#!venv/bin/python
 if __name__ == '__main__':
     print("Ciao, tutto funziona correttamente!!")
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='hellosnap',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='carlo',
+    author_email='',
+    description=''
+)
+
